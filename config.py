79822a70b08ca79f1a4d2f58d460e114a0e89401
datstar2 <pssd12@gmail.com>
2021-01-12 11:33:23 +0900

    dfdf

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ JWT_SECRET_KEY        = 'SOME_SUPER_SECRET_KEY'
 JWT_EXP_DELTA_SECONDS = 7 * 24 * 60 * 60
 
 S3_BUCKET     = "beetbitbucket"
-S3_ACCESS_KEY = "AKIAWP6LXW45RHJWSNMS"
-S3_SECRET_KEY = "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"
+S3_ACCESS_KEY = "*"
+S3_SECRET_KEY = "*"
 S3_BUCKET_URL = f"https://s3.ap-northeast-2.amazonaws.com/{S3_BUCKET}/"
 
 def thread_run():
